fdcf05190d774ba5af36aca6ede51c3db59bb27d
Egbon Of Crypto <112263576+kizzydez@users.noreply.github.com>
2025-10-15 12:16:49 +0100

    Update and rename route.js to route.ts

--- app/api/wallet/cached/route.ts
+++ app/api/wallet/cached/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 // app/api/wallet/cached/route.ts
 import { type NextRequest, NextResponse } from "next/server"
 import { createClient } from "@/lib/supabase/server"
